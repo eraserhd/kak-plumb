8449528bbf4780f90c15075f8d1cb9adcd0969dc
Jason Felice <jason.m.felice@gmail.com>
2019-10-10 10:23:46 -0400

    Make release.nix run tests

--- release.nix
+++ release.nix
@@ -3,8 +3,17 @@ let
   pkgs = import nixpkgs { config = {}; };
   kak-plumb = pkgs.callPackage ./derivation.nix {};
 in {
-  test = pkgs.runCommandNoCC "kak-plumb-test" {} ''
-    mkdir -p $out
-    : ${kak-plumb}
-  '';
+  test = pkgs.stdenv.mkDerivation {
+    name = "kak-plumb-test-2019.10.10";
+    src = ./.;
+
+    buildInputs = [ pkgs.bash pkgs.kakoune-unwrapped ];
+    buildPhase = ''
+      patchShebangs --build test/9
+      ${pkgs.bash}/bin/bash test/tests.bash
+    '';
+    installPhase = ''
+      mkdir -p "$out"
+    '';
+  };
 }
